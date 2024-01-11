ad349c44f59791f406223cbf7a3a18ae49f27368
Y4chi <Y4chi2023@gmail.com>
2024-01-11 17:57:28 -0600

    V1.1

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/y4chi/Class-Python/Django/.venv"
+set -gx VIRTUAL_ENV "/home/y4chi/Class-Python/SpaceSearch/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
